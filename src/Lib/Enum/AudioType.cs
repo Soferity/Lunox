aa3a04bb6fb89ab82da7a91b1bdf8ac3075b97c3
Taiizor <taiizor@soferity.com>
2022-01-07 00:49:42 +0300

    Summary Entry

--- src/Lib/Enum/AudioType.cs
+++ src/Lib/Enum/AudioType.cs
@@ -8,25 +8,64 @@
     public enum AudioType
     {
         /// <summary>
-        /// 
+        /// An MP3 file is an audio file saved in a compressed audio format developed by the
+        /// Moving Picture Experts Group (MPEG) that uses "Layer 3" audio compression (MP3).
+        /// MP3 files are commonly used to store audio tracks, podcasts, lectures, sermons,
+        /// and audiobooks.
         /// </summary>
         MP3,
         /// <summary>
-        /// 
+        /// An M4A file is an audio file that may store various types of audio content,
+        /// such as songs, podcasts, and audiobooks. It is saved in the MPEG-4 format and
+        /// encoded with either the Advanced Audio Coding (AAC) codec or the Apple Lossless
+        /// Audio Codec (ALAC).
+        /// </summary>
+        M4A,
+        /// <summary>
+        /// An AAC file is an audio file similar to an .MP3 file but compressed with Advanced
+        /// Audio Coding (AAC), a lossy digital audio compression standard. The lossy compression
+        /// maintains quality nearly indistinguishable from the original audio source, making it
+        /// optimal for compressing music data.
         /// </summary>
         AAC,
         /// <summary>
-        /// 
+        /// A FLAC file is an audio file compressed in the Free Lossless Audio Codec (FLAC) format,
+        /// which is an open-source lossless audio compression format. It is similar to an .MP3 file,
+        /// but is compressed without any loss in quality or loss of any original audio data.
+        /// </summary>
+        FLAC,
+        /// <summary>
+        /// Audio file created with the Apple Lossless Audio Codec (ALAC); used for storing digital
+        /// music losslessly without losing any sound quality from the original audio data; differs
+        /// from lossy audio codecs such as AAC.
         /// </summary>
         ALAC,
         /// <summary>
-        /// 
+        /// An AIFF file is an audio file saved in the Audio Interchange File Format (AIFF). It contains
+        /// high-quality audio stored in an uncompressed lossless format. AIFF files may also be saved as
+        /// .AIF files or .AIFC files (if they are compressed).
+        /// </summary>
+        AIFF,
+        /// <summary>
+        /// A WAV file is an audio file saved in the WAVE format, a standard digital audio file format
+        /// utilized for storing waveform data. WAV files may contain audio recordings with different
+        /// sampling rates and bitrates but are often saved in a 44.1 kHz, 16-bit, stereo format, which
+        /// is the standard format used for CD audio.
         /// </summary>
         WAV,
         /// <summary>
-        /// 
+        /// A WMA file is an audio file saved in the Advanced Systems Format (ASF) container format
+        /// developed by Microsoft. It stores audio data encoded in the Windows Media Audio (WMA),
+        /// WMA Pro, WMA Lossless, or WMA Voice codecs. WMA files also include metadata objects such
+        /// as the title, artist, album, and genre of the track.
         /// </summary>
-        WMA
+        WMA,
+        /// <summary>
+        /// An OGG file is an audio file, similar to an .MP3 file, that typically stores music. It saves
+        /// audio data in the Ogg container format that is compressed with the free, unpatented Vorbis
+        /// audio compression. It may also include song metadata, such as artist information and track data.
+        /// </summary>
+        OGG
     }
 
     #endregion
--- src/Lib/Enum/VideoType.cs
+++ src/Lib/Enum/VideoType.cs
@@ -8,25 +8,57 @@
     public enum VideoType
     {
         /// <summary>
-        /// 
+        /// An MP4 file is a multimedia file that stores a movie or video clip and may also contain
+        /// subtitles, images, and metadata. It is one of the most common formats used for distributing
+        /// video online, whether it be streaming or sharing videos.
         /// </summary>
         MP4,
         /// <summary>
-        /// 
+        /// An AVI file is a video file saved in the Audio Video Interleave (AVI) multimedia container
+        /// format created by Microsoft. It stores video and audio data encoded in various codecs,
+        /// including DivX and XviD.
         /// </summary>
         AVI,
         /// <summary>
-        /// 
+        /// An HEVC file contains a video stored in the High Efficiency Video Coding (HEVC) format.
+        /// This format, also known as H.265, improves over the H.264 standard by allowing videos to
+        /// be stored with a lower file size but with the same video quality. HEVC helps users store
+        /// more videos on their devices and also substantially reduces the file size of high-resolution
+        /// videos such as 4K and 8K videos.
         /// </summary>
         HEVC,
         /// <summary>
-        /// 
+        /// A WMV file is a video stored in the Microsoft Advanced Systems Format (ASF) and compressed
+        /// with Windows Media Video compression. It is meant to be played in Microsoft media players,
+        /// such as Windows Media Player and Microsoft Movies & TV. WMV files may be encrypted for use
+        /// with Digital Rights Management (DRM) systems.
         /// </summary>
         WMV,
         /// <summary>
-        /// 
+        /// An MPEG file is a video file that uses a digital video format standardized by the Moving
+        /// Picture Experts Group (MPEG). It contains video and audio that are compressed using MPEG-1
+        /// or MPEG-2 compression. MPEG files are typically used to share videos over the Internet.
         /// </summary>
-        MPEG
+        MPEG,
+        /// <summary>
+        /// Video file compressed using the MPEG-1 codec; may include both audio and video, though the
+        /// audio track may use a separate type of compression; can be opened and played back by most
+        /// media players
+        /// </summary>
+        MPEG1,
+        /// <summary>
+        /// An MPEG2 file is a video file encoded using the MPEG-2 codec, which is typically used to
+        /// compress over-the-air, satellite, and cable TV transmissions, as well as DVD video. It is
+        /// compressed using lossy compression, which significantly reduces the size of the video and
+        /// audio data it contains. MPEG-2 compression is less efficient than H.264 (.H264) compression.
+        /// </summary>
+        MPEG2,
+        /// <summary>
+        /// An MPEG4 file is a video file saved in the MPEG-4 container format. It includes both audio
+        /// and video data and supports multiple A/V codecs. MPEG4 files are commonly used for distributing
+        /// video content over the web and for streaming videos on the Internet.
+        /// </summary>
+        MPEG4
     }
 
     #endregion
