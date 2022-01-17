6dad97369b3c8b2557197ce378f75595aa392c82
Taiizor <taiizor@soferity.com>
2022-01-17 18:55:26 +0300

    Navigation Preparing

---
+++ src/Lib/Enum/CascadingStyleSheetsType.cs
@@ -0,0 +1,21 @@
+﻿namespace Lunox.Library.Enum
+{
+    #region CascadingStyleSheetsTypeEnum
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum CascadingStyleSheetsType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Minify,
+        /// <summary>
+        /// 
+        /// </summary>
+        Beautify
+    }
+
+    #endregion
+}
---
+++ src/Lib/Enum/JavaScriptType.cs
@@ -0,0 +1,21 @@
+﻿namespace Lunox.Library.Enum
+{
+    #region JavaScriptTypeEnum
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum JavaScriptType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Minify,
+        /// <summary>
+        /// 
+        /// </summary>
+        Beautify
+    }
+
+    #endregion
+}
