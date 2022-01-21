7df81f5541cdb9643a237711a0a1200f5524e9e8
Taiizor <taiizor@soferity.com>
2022-01-22 01:02:40 +0300

    JsonSerialize Preparing

---
+++ src/Core/ViewModels/Json/SerializeViewModel.cs
@@ -0,0 +1,29 @@
+﻿#region Imports
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+#endregion
+
+namespace Lunox.Core.ViewModels.Json
+{
+    #region SerializeViewModel
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SerializeViewModel : ObservableObject
+    {
+        #region Functions
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SerializeViewModel()
+        {
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ src/Core/Views/Json/SerializePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿#region Imports
+
+using Lunox.Core.ViewModels.Json;
+using Windows.UI.Xaml.Controls;
+
+#endregion
+
+namespace Lunox.Core.Views.Json
+{
+    #region SerializePage
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class SerializePage : Page
+    {
+        #region Functions
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SerializeViewModel ViewModel { get; } = new SerializeViewModel();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SerializePage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
